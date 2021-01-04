ea37b91ecae774f38c18c71d44849c6a6674fba7
RayWang <2271272653@qq.com>
2021-01-04 18:18:39 +0800

    Init

---
+++ src/Ray.Infrastructure/Config/EnvironmentVariablesExcludeEmptyConfigurationSource.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Ray.Infrastructure.Config
+{
+    /// <summary>
+    /// 自定义的排除空值的环境变量配置源
+    /// （用于取待默认的<see cref="EnvironmentVariablesConfigurationSource"/>）
+    /// </summary>
+    public class EnvironmentVariablesExcludeEmptyConfigurationSource : IConfigurationSource
+    {
+        public string Prefix { get; set; }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new EnvironmentVariablesExcludeEmptyConfigurationProvider(Prefix);
+        }
+    }
+}
---
+++ src/Ray.Infrastructure/Global.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Ray.Infrastructure
+{
+    public class Global
+    {
+        /// <summary>
+        /// 环境Key
+        /// </summary>
+        public static string EnvironmentKey = "ASPNETCORE_ENVIRONMENT";
+
+        /// <summary>
+        /// 环境
+        /// </summary>
+        public static IHostEnvironment HostingEnvironment { get; set; }
+
+        /// <summary>
+        /// 根配置
+        /// </summary>
+        public static IConfigurationRoot ConfigurationRoot { get; set; }
+
+        /// <summary>
+        /// 根容器
+        /// </summary>
+        public static IServiceProvider ServiceProviderRoot { get; set; }
+    }
+}
