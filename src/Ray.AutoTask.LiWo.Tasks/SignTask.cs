81b48c5c83737de59d79daf61e545f0d8ec976a6
RayWang <2271272653@qq.com>
2021-01-06 18:46:53 +0800

    Update

---
+++ src/Ray.AutoTask.LiWo.Tasks/SignTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Ray.AutoTask.LiWo.Tasks
+{
+    public class SignTask
+    {
+        public SignTask()
+        {
+
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
