3316ce74be1d8d08a67a65c7a040f3ae146a5f41
RayWang <2271272653@qq.com>
2021-06-21 18:32:56 +0800

    Update

---
+++ samples/BiliBiliTool/src/Hangfire.Dashboard.Extensions/Dispatchers/CombinedResourceDispatcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire.Annotations;
+
+namespace Hangfire.Dashboard.Extensions.Dispatchers
+{
+    public class CombinedResourceDispatcher : EmbeddedResourceDispatcher
+    {
+        private readonly Assembly _assembly;
+        private readonly string _baseNamespace;
+        private readonly string[] _resourceNames;
+
+        public CombinedResourceDispatcher(
+            [NotNull] string contentType,
+            [NotNull] Assembly assembly,
+            string baseNamespace,
+            params string[] resourceNames) : base(contentType, assembly, null)
+        {
+            _assembly = assembly;
+            _baseNamespace = baseNamespace;
+            _resourceNames = resourceNames;
+        }
+
+        protected override async Task WriteResponse(DashboardResponse response)
+        {
+            foreach (var resourceName in _resourceNames)
+            {
+                await WriteResource(
+                    response,
+                    _assembly,
+                    $"{_baseNamespace}.{resourceName}").ConfigureAwait(false);
+            }
+        }
+    }
+}
