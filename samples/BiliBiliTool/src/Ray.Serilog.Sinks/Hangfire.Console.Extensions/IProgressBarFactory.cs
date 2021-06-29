4770085ebf1af26f63a103eef09f02f85bd5a239
RayWang <2271272653@qq.com>
2021-06-30 00:18:06 +0800

    Update

--- samples/BiliBiliTool/src/Ray.Serilog.Sinks/Hangfire.Console.Extensions/IProgressBarFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Hangfire.Console.Progress;
-
-namespace Hangfire.Console.Extensions
-{
-    public interface IProgressBarFactory
-    {
-        IProgressBar Create(string name = null, double value = 0, ConsoleTextColor color = null);
-    }
-}
